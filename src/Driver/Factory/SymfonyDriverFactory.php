15f9674c2169af07957bc1fbf9bff05dfb04c542
Łukasz Chruściel <lchrusciel@gmail.com>
2016-07-23 09:58:27 +0200

    Add license blocks

--- src/Driver/Factory/SymfonyDriverFactory.php
+++ src/Driver/Factory/SymfonyDriverFactory.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the SymfonyExtension package.
+ *
+ * (c) FriendsOfBehat
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace FriendsOfBehat\SymfonyExtension\Driver\Factory;
 
 use Behat\MinkExtension\ServiceContainer\Driver\DriverFactory;
