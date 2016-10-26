8c143d3ba9da8bda804fd4c2286f44587e9c2d38
Kamil Kokot <kamil@kokot.me>
2016-10-26 10:54:20 +0200

    Update copyright holder in license header

--- src/Listener/KernelRebooter.php
+++ src/Listener/KernelRebooter.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the SymfonyExtension package.
  *
- * (c) FriendsOfBehat
+ * (c) Kamil Kokot <kamil@kokot.me>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
