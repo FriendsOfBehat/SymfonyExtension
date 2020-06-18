394c417675bcce136ef7be76848ee65ba9245fd6
cv65kr <cv65kr@gmail.com>
2020-06-18 20:24:36 +0200

    Missing class for Symfony 5

--- src/Bundle/DependencyInjection/FriendsOfBehatSymfonyExtensionExtension.php
+++ src/Bundle/DependencyInjection/FriendsOfBehatSymfonyExtensionExtension.php
@@ -10,7 +10,6 @@ use Behat\Mink\Session;
 use FriendsOfBehat\SymfonyExtension\Mink\MinkParameters;
 use FriendsOfBehat\SymfonyExtension\ServiceContainer\SymfonyExtension;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
-use Symfony\Component\BrowserKit\Client;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -79,9 +78,12 @@ final class FriendsOfBehatSymfonyExtensionExtension extends Extension implements
             return;
         }
 
-        foreach ([Client::class, KernelBrowser::class, HttpKernelBrowser::class] as $class) {
-            $container->setAlias($class, 'test.client');
+        if (\class_exists('Symfony\Component\BrowserKit\Client')) {
+            $container->setAlias('Symfony\Component\BrowserKit\Client', 'test.client');
         }
+
+        $container->setAlias(KernelBrowser::class, 'test.client');
+        $container->setAlias(HttpKernelBrowser::class, 'test.client');
     }
 
     private function provideMinkIntegration(ContainerBuilder $container): void
