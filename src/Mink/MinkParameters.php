8a48661921a6abdda21d65864b67235a6af4b02a
Ruud Kamphuis <ruudk@users.noreply.github.com>
2021-11-13 10:23:18 +0100

    Add PHP 8.1 compatibility
    Return type of FriendsOfBehat\SymfonyExtension\Mink\MinkParameters::offsetGet($offset) should either be compatible with ArrayAccess::offsetGet(mixed $offset): mixed, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice

--- src/Mink/MinkParameters.php
+++ src/Mink/MinkParameters.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace FriendsOfBehat\SymfonyExtension\Mink;
 
+use ReturnTypeWillChange;
+
 /** @final */
 class MinkParameters implements \Countable, \IteratorAggregate, \ArrayAccess
 {
@@ -25,6 +27,7 @@ class MinkParameters implements \Countable, \IteratorAggregate, \ArrayAccess
         return array_key_exists($offset, $this->minkParameters);
     }
 
+    #[ReturnTypeWillChange]
     public function offsetGet($offset)
     {
         return $this->minkParameters[$offset] ?? null;
